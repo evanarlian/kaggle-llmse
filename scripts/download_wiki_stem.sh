2255ffb0bb980252bf37023f9b75630772bf5143
Evan Arlian <evanarlian2000@gmail.com>
2023-09-18 23:29:46 +0700

    remove stem wiki

--- scripts/download_wiki_stem.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# https://www.kaggle.com/datasets/nbroad/wiki-20220301-en-sci
-kaggle datasets download -d nbroad/wiki-20220301-en-sci -p input/wiki-20220301-en-sci
-cd input/wiki-20220301-en-sci
-unzip wiki-20220301-en-sci.zip
