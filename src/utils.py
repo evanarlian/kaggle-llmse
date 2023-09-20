48de71d476078f8e9e67fd201985c2ddf848ceca
Evan Arlian <evanarlian2000@gmail.com>
2023-09-20 13:55:44 +0700

    add extended dir utils

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,5 @@
+import re
+
 import pandas as pd
 from IPython.display import display
 
@@ -5,3 +7,11 @@ from IPython.display import display
 def wider(df: pd.DataFrame):
     with pd.option_context("display.max_colwidth", None):
         display(df)
+
+
+def dirx(obj, pat=None):
+    """dir() extended, can filter by regex."""
+    members = dir(obj)
+    if pat is not None:
+        members = [m for m in members if re.search(pat, m)]
+    return members
