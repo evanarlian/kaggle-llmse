67a0a13d79c4a9c517c452a8abac09422ba4194a
Evan Arlian <evanarlian2000@gmail.com>
2023-09-21 08:36:57 +0700

    use less memory

--- src/paragraph_level_emb.py
+++ src/paragraph_level_emb.py
@@ -84,17 +84,20 @@ def create_faiss(model: SentenceTransformer, ds: Dataset, title_trick: bool) -> 
             return {"pars": row["text"]}
         paragraphs = ds.map(_notrick, remove_columns=["title", "text"])
     # fmt: on
-    all_embeddings = []
+    d = 384
+    # np concat consumes huge memory so we can't do that
+    all_embeddings = np.ones((len(paragraphs), d), dtype=np.float32)
+    count = 0
     for batch in tqdm(paragraphs.iter(batch_size=128), desc="Embed"):
         pars = batch["pars"]
         features = model.tokenize(pars)
         features = {k: v.cuda() for k, v in features.items()}
         out_features = model.forward(features)
         embeddings = out_features["sentence_embedding"]
-        all_embeddings.append(embeddings.cpu())
-    all_embeddings = torch.cat(all_embeddings, dim=0).numpy()
+        all_embeddings[count : count + len(pars)] = embeddings.cpu().numpy()
+        count += len(pars)
     # no need to normalize embeddings since we want to use IndexFlatIP (dot prod)
-    index = faiss.IndexFlatIP(all_embeddings.shape[1])
+    index = faiss.IndexFlatIP(d)
     index.add(all_embeddings)
     savepath = f"input/llmse-paragraph-level-emb-faiss/wiki_{'trick' if title_trick else 'notrick'}.index"
     faiss.write_index(index, savepath)
